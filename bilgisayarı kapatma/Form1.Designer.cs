59f90204673a3ee059f9aca1207bbc188fa5ffb2
Panaria <baranyldzby@gmail.com>
2023-10-23 20:50:54 +0300

    Add project files.

---
+++ bilgisayarı kapatma/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace bilgisayarı_kapatma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Image = global::bilgisayarı_kapatma.Properties.Resources.Other_Power_Restart_Metro_icon;
+            this.button2.Location = new System.Drawing.Point(434, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(259, 241);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Image = global::bilgisayarı_kapatma.Properties.Resources.MB_0019_shut_down_kapat;
+            this.button1.Location = new System.Drawing.Point(114, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 241);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(173, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bilgisayarı Kapat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(444, 336);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bilgisayarı Yeniden Başlat";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ bilgisayarı kapatma/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bilgisayarı_kapatma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult sor;
+            sor = MessageBox.Show("Bilgisayar Kapatılsın mı?" , "Uyarı" , MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (sor == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("shutdown", "-s");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult sor;
+            sor = MessageBox.Show("Bilgisayar Yeniden Başlatılsın mı?", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (sor == DialogResult.Yes)
+            {
+                System.Diagnostics.Process.Start("shutdown", "-r");
+            }
+        }
+    }
+}
